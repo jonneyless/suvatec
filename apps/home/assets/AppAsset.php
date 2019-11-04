1cd2ed962ca26d28755d2d3ccf218a4eaee1af16
Jony <jonneyless@163.com>
2019-11-04 22:03:25 +0800

    sns

--- apps/home/assets/AppAsset.php
+++ apps/home/assets/AppAsset.php
@@ -12,6 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'https://cdn.bootcss.com/font-awesome/5.11.2/css/brands.min.css',
         'css/site.css',
     ];
     public $js = [
