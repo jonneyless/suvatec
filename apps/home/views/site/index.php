6f5a03104072b1a9b88f9032ef1d0ca67259f80f
Jony <jonneyless@163.com>
2020-01-13 22:35:34 +0800

    调整一下

--- apps/home/views/site/index.php
+++ apps/home/views/site/index.php
@@ -19,71 +19,71 @@ $this->registerMetaTag(['name' => 'description', 'content' => 'Suvatec also can 
     </div>
 
     <div class="about-us box">
-        <div class="box-header">
+        <h1 class="box-header">
             About Us
-        </div>
+        </h1>
 
         <div class="box-body">
-        <div class="container">
-            <div class="clearfix">
-                <div class="left-item">
-                    <div class="image"></div>
-                </div>
+            <div class="container">
+                <div class="clearfix">
+                    <div class="left-item">
+                        <div class="image"></div>
+                    </div>
 
-                <div class="right-item">
-                    <div class="text">
+                    <div class="right-item">
+                        <div class="text">
                         <h3>About Suvatec</h3>
                         <?php $aboutUs = Page::findOne(1); ?>
                         <p><?= $aboutUs->showSummery() ?></p>
                         <a href="<?= $aboutUs->getViewUrl() ?>" class="more">LEARN MORE</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="service box">
-    <div class="box-header">
-        Sourcing and Supply Chain Management Service
-    </div>
+    <div class="service box">
+        <h1 class="box-header">
+            Sourcing and Supply Chain Management Service
+        </h1>
 
-    <div class="box-body">
-        <div class="container">
-            <div class="icons">
-                <?php for ($i = 1; $i <= 6; $i++) { ?>
+        <div class="box-body">
+            <div class="container">
+                <div class="icons">
+                    <?php for ($i = 1; $i <= 6; $i++) { ?>
                     <div class="icon<?= $i ?>">
                         <a href="<?= Service::findOne($i)->getViewUrl() ?>"></a>
                     </div>
                 <?php } ?>
+                </div>
+                Suvatec team has been in electronic industry since 2008, beginning with PCB manufacturing, assembling, then following the market needs and make the best out of our capability, to electronic turn key projects management.
+                With the great advantage of Supply China in China Pearl River Delta(China electronic manufacturing base), we provide professional Sourcing and Supply Chain Management Service. Our experience in the industry, professional skills and great resouces here could be your shortcut to mature technology and product.In the mean time, save your investment and hassle of coordinating among different suppliers.
             </div>
-            Suvatec team has been in electronic industry since 2008, beginning with PCB manufacturing, assembling, then following the market needs and make the best out of our capability, to electronic turn key projects management.
-            With the great advantage of Supply China in China Pearl River Delta(China electronic manufacturing base), we provide professional Sourcing and Supply Chain Management Service. Our experience in the industry, professional skills and great resouces here could be your shortcut to mature technology and product.In the mean time, save your investment and hassle of coordinating among different suppliers.
         </div>
     </div>
-</div>
 
-<div class="oem box">
-    <div class="container">
-        <div class="box-header">
-            OEM / Sourcing Service
+    <div class="oem box">
+        <div class="container">
+            <h1 class="box-header">
+                OEM / Sourcing Service
+            </h1>
+
+            <div class="box-body">
+                Build a product from scratch takes time and funds, thus we provide certain model ready products available with customized logo and color,more than that, we provide sourcing service according to your special needs in electronics.
+            </div>
         </div>
+    </div>
+
+    <div class="box">
+        <h1 class="box-header">
+            Star Product
+        </h1>
 
         <div class="box-body">
-            Build a product from scratch takes time and funds, thus we provide certain model ready products available with customized logo and color,more than that, we provide sourcing service according to your special needs in electronics.
-        </div>
-    </div>
-</div>
-
-<div class="box">
-    <div class="box-header">
-        Star Product
-    </div>
-
-    <div class="box-body">
-        <div class="container">
-            <div class="column-list">
-                <?php foreach ($starProducts as $product) { ?>
+            <div class="container">
+                <div class="column-list">
+                    <?php foreach ($starProducts as $product) { ?>
                 <div class="column-flex-1">
                     <a href="<?= $product->getViewUrl() ?>">
                         <img src="<?= $product->getPreview(368, 368, false) ?>"/>
@@ -91,33 +91,33 @@ $this->registerMetaTag(['name' => 'description', 'content' => 'Suvatec also can 
                     <p><?= $product->name ?></p>
                 </div>
                 <?php } ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="box">
-    <div class="box-header">
-        Our Clients
-    </div>
+    <div class="box">
+        <h1 class="box-header">
+            Our Clients
+        </h1>
 
-    <div class="box-body">
-        <div class="container">
-            <div class="our-clients">
-                <a href="javascript:;" class="client-item client-item-1"></a>
-                <a href="javascript:;" class="client-item client-item-2"></a>
-                <a href="javascript:;" class="client-item client-item-3"></a>
-                <a href="javascript:;" class="client-item client-item-4"></a>
-                <a href="javascript:;" class="client-item client-item-5"></a>
+        <div class="box-body">
+            <div class="container">
+                <div class="our-clients">
+                    <a href="javascript:;" class="client-item client-item-1"></a>
+                    <a href="javascript:;" class="client-item client-item-2"></a>
+                    <a href="javascript:;" class="client-item client-item-3"></a>
+                    <a href="javascript:;" class="client-item client-item-4"></a>
+                    <a href="javascript:;" class="client-item client-item-5"></a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
     <div class="contect box">
-        <div class="box-header">
-            WANNA BUILD A PRODUCT?
-        </div>
+        <h1 class="box-header">
+            Wanna Build a Product?
+        </h1>
 
         <div class="box-body">
             <div class="container">
