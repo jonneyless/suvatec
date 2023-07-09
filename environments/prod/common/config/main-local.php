6861b645f82ef19f1f194c079adda74e50387780
Jony <jonneyless@163.com>
2023-07-09 11:17:02 +0800

    更新

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=suvatec',
             'username' => 'root',
-            'password' => '',
+            'password' => 'yangzhou07',
             'charset' => 'utf8',
         ],
         'mailer' => [
